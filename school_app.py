b94fff628923ae39b66f6b3763f4e90568c2a240
aghatem <69748322+aghatem@users.noreply.github.com>
2022-01-21 19:07:34 +0000

    Update school_app.py

--- school_app.py
+++ school_app.py
@@ -1,24 +1,35 @@
 
+
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
+import requests
+from math import sin, cos, sqrt, atan2, radians
+import math
+import pandas as pd
+import chardet
+
+# Streamlit documentation https://docs.streamlit.io/library/api-reference/widgets
 def GUI():
 	st.title('UK Schools checker App')
 # get user Post code
-	st.sidebar.subheader("User Details")
+	st.sidebar.subheader("User Post Code")
 	user_post = st.sidebar.text_input("Please enter your post code: ", "WC1B3DG")
 	button_was_clicked = st.sidebar.button("SUBMIT")
 	user_crd = Post_Code_to_Coordinates(user_post)
 	user_dist = user_post[0:3]
 	st.write('Your coordinates are : ', user_crd[0], " & ",  user_crd[1])
-	st.write('Your District is  : ', user_dist )
+	st.write('Your district is in : ', user_dist )
 	#Choose mean of transport
 	transport = ( "Car", "Public Transport", "Walking", "Bike")
 	selected_Transport = st.sidebar.selectbox('Enter the mean of transport', transport)
-	return(user_post,user_dist,user_crd)
+	schools = ("sch1","sch2")
+	return user_post, user_dist ,user_crd
 	
 # get user coordinates
 def Post_Code_to_Coordinates(pcode):
 
-    import requests
+   
     coord_API = "http://api.getthedata.com/postcode/"
     c_r = requests.get(coord_API+pcode)
     #print(c_r.json())
@@ -30,9 +41,7 @@ def Post_Code_to_Coordinates(pcode):
     return lat, long
 
 def mapit(lat,long):
-	import streamlit as st
-	from streamlit_folium import folium_static
-	import folium
+
 	# center the map on British Muserum WC1B3DG
 	global m
 	m = folium.Map(location=[lat , long], tiles="OpenStreetMap",zoom_start=16)
@@ -43,10 +52,34 @@ def mapit(lat,long):
 	folium.Marker([51.373039, -0.204720], popup="Liberty Bell", tooltip=tooltip).add_to(m)
 	folium_static(m)
 
+def Travel_GAPI(Lat1,Long1,Lat2,Long2,Transport_mode):
+	
+	# Google Distance API Documentation https://developers.google.com/maps/documentation/distance-matrix/overview?hl=en_US#transit_mode
+	source = Lat1 & Long1
+	Dest = Lat2 & Long2
+	API_key= open("keyfile.txt", "r").read()
+	if tranport_mode == 'car':
+
+		url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key="&API_key&driving
+	if tranport_mode == 'Public Transport':
+
+		url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key="&API_key&transit
+	if tranport_mode == 'Walking':
+
+		url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key="&API_key&walking
+	if tranport_mode == 'Bike':
+
+		url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key="&API_key&bicycling
+
+	payload={}
+	headers = {}
+
+	response = requests.request("GET", url, headers=headers, data=payload)
+
+	return (response.text)
 
 def get_distance (lat1,lon1,lat2,lon2):
-    from math import sin, cos, sqrt, atan2, radians
-    import math
+
     R = 6373.0
 
     lati1 = radians(lat1)
@@ -64,10 +97,7 @@ def get_distance (lat1,lon1,lat2,lon2):
     return distance
 
 def nearby_schools(po,user_crd):
-	from streamlit_folium import folium_static
-	import folium
-	import pandas as pd
-	import chardet
+
 
 	schools = pd.read_csv('schools_db.csv', engine='python', encoding='ISO-8859-1')
 	# Insert a new column in the df located in 1st column with name Distrocs and data to be added	
@@ -80,7 +110,7 @@ def nearby_schools(po,user_crd):
 	selected_schools = df[[ 'EstablishmentName', 'OfstedRating (name)', 'LA (name)', 'SchoolCapacity', 'Postcode', 'Locality', 'NumberOfPupils', 'NumberOfBoys',
 	 'NumberOfGirls', 'DistrictAdministrative (name)', 'Gender (name)', 'AdministrativeWard (name)', 'PhaseOfEducation (name)', 'ReligiousCharacter (name)',
 	  'StatutoryLowAge', 'StatutoryHighAge', 'TypeOfEstablishment (name)', 'OfstedLastInsp', 'Street', 'Address3', 'Town', 'County (name)', 'SchoolWebsite',
-	   'TelephoneNum', 'EstablishmentStatus (name)', 'UKPRN', 'HeadFirstName', 'HeadLastName']]
+	   'TelephoneNum', 'EstablishmentStatus (name)', 'NurseryProvision (name)', 'UKPRN', 'HeadFirstName', 'HeadLastName']]
 	# Adding coordinates to each school
 	selected_schools.insert(23, "lat", True)
 	selected_schools.insert(24, "long", True)
@@ -91,43 +121,36 @@ def nearby_schools(po,user_crd):
 		selected_schools.iloc[i,23] = Post_Code_to_Coordinates(selected_schools.iloc[i,4])[0]
 		selected_schools.iloc[i,24] = Post_Code_to_Coordinates(selected_schools.iloc[i,4])[1]
 		selected_schools.iloc[i,25] = get_distance(float(selected_schools.iloc[i,23]),float(selected_schools.iloc[i,24]),float(user_crd[0]),float(user_crd[1]))
-		#folium.Marker(location=[float(selected_schools.iloc[i,23]),float(selected_schools.iloc[i,24])],popup=selected_schools.iloc[i,0]).add_to(m)
-	
-	
-	
-	import streamlit as st
-	from streamlit_folium import folium_static
-	import folium
+		
+
 	
 	global m
 	m = folium.Map(location=[user_crd[0] , user_crd[1]], tiles="OpenStreetMap",zoom_start=14)
 	tooltip = "" 
 	folium.Marker([user_crd[0] , user_crd[1]],color='black' ,popup="Your Home",icon=folium.Icon(color='green', icon_color='white', icon='tint')).add_to(m)
 	for i in range (0,len(selected_schools)):
+		
 		folium.Marker([float(selected_schools.iloc[i,23]),float(selected_schools.iloc[i,24])], popup=selected_schools.iloc[i,0]).add_to(m)
 	
-
-
-	folium_static(m)
-
 	st.write(selected_schools.head(20))
 	
-	#st.write(selected_schools['Distance'].idxmin())
-	#st.write(selected_schools.loc[selected_schools['Distance'].idxmin()])
+
 	st.write("Nearest School Name is far ", selected_schools['Distance'].min(), " meters from your home")
-	#show schools statistics in this district
 	st.write('Number of schools in youd district is :', len(selected_schools))
 	st.write(selected_schools.value_counts('OfstedRating (name)'))
+	#schools = selected_schools['EstablishmentName'].tolist()
+	#st.write('Selected School:', option)
+	newdf = selected_schools.loc[(selected_schools.EstablishmentName == option)]
+	Travel_GAPI(user_crd[0] , user_crd[1],newdf[0,23],newdf[0,24],selected_Transport)
 
 def main ():
-	from streamlit_folium import folium_static
 	
 	x = GUI()
 	user_dist = x[1]
 	user_post = x[0]
 	user_crd = x[2]
 	#mapit(user_crd[0], user_crd[1])
-
+	
 	nearby_schools(user_post,user_crd)
 	
 
